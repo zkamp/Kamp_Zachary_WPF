15ffdcb3a70af65c6e2b45f1f1aae342523c03f1
zkamp <zrkamp@fullsail.edu>
2014-12-04 17:56:00 -0500

    Asked the user for birth year.
    Asked the user for birth year.

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -15,3 +15,10 @@ var name = prompt("Please type your name");
 
 //use the variable and say hello to the user
 alert("Welcome "+name+"! Let's figure out how old you are.");
+
+//ask the user what year they were born
+//create a variable to catch that answer
+var yearBorn = prompt("What year were you born");
+
+//Console.log to check what they type
+console.log(yearBorn);S
