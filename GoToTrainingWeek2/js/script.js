6db0106ad5da63ae4f0315f5298ef7e82ecc932c
zkamp <zrkamp@fullsail.edu>
2014-12-04 17:46:54 -0500

    Prompted User's name
    Prompted User's name

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -6,4 +6,12 @@ Go To Training Week #2
  */
 
 
-alert("testing");
+//alert("testing");
+
+// create an age calculator
+
+//Ask the user their name
+var name = prompt("Please type your name");
+
+//use the variable and say hello to the user
+alert("Welcome "+name+"! Let's figure out how old you are.");
