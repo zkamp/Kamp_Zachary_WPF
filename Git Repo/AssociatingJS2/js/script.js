007fac0a11db90d0ff1b9b4c1468e8a573210e06
zkamp <zrkamp@fullsail.edu>
2014-12-04 16:15:03 -0500

    Week 2
    Week 2 screencast activities.

---
+++ Git Repo/AssociatingJS2/js/script.js
@@ -0,0 +1,8 @@
+//Prompt and Alert
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter the height");
+var area = width * height; //calculates with that info
+//console.log(area);//prints it out to console
+
+alert("The area of your rectangle is " + area + " sq feet");
