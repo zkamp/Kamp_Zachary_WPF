2e9cf9b67ecf19ee53a2190e1e022d9984c58455
zkamp <zrkamp@fullsail.edu>
2014-12-04 18:24:43 -0500

    Finished age calculator.
    Finished age calculator.

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -38,3 +38,13 @@ alert(name+" you are "+age+" years old.");
 var yearsMore = prompt("How many years in the future would you like to know?");
 
 console.log(yearsMore);
+
+//Calculate their future age
+//Cast the returned variable from a text string to a number
+//Number(thing to cast goes here)
+var futureAge = age + Number(yearsMore);
+console.log(futureAge);
+
+//Make the results look pretty
+console.log("You will be "+futureAge+" in "+yearsMore+" years.");
+alert("You will be "+futureAge+" in "+yearsMore+" years.");
