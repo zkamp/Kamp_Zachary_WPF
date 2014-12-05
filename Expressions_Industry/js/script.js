0235bcedebc505f4dbbde66968f51ec6c6ba5777
zkamp <zrkamp@fullsail.edu>
2014-12-04 21:11:30 -0500

    Days for a site build
    Days for a site build

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,7 @@
  12-4-2014
  */
 //alert("This is Industry!");
+
+//How many websites can be built in a year
+//How long for one site
+var siteTime = prompt("How many days (on average) does it take you to build a website?");
