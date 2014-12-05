150c444d655a77e764cd3bd3b6930f390486b938
zkamp <zrkamp@fullsail.edu>
2014-12-04 22:34:48 -0500

    Log answer for first prompt
    Log answer for first prompt

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,4 +4,11 @@
  Go To Training Week #2
  12-4-2014
  */
-alert("This is Wacky!");
+
+//alert("This is Wacky!");
+
+//people in house
+var peopleInHouse = ("How many people live in your house?");
+
+//log number
+console.log(peopleInHouse);
