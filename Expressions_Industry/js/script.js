c3ed33138c8facbcc938d611cf35da8892db4c60
zkamp <zrkamp@fullsail.edu>
2014-12-04 21:18:31 -0500

    Log days in console
    Log days in console

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,3 +9,8 @@
 //How many websites can be built in a year
 //How long for one site
 var siteTime = prompt("How many days (on average) does it take you to build a website?");
+
+//log days in the console
+console.log(siteTime);
+
+
