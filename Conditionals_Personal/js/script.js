0dd1148e51409b5e6eddeb22ec01beaa22289f64
zkamp <zrkamp@fullsail.edu>
2014-12-11 13:52:43 -0500

    Creating coffee calculator
    Creating coffee calculator

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,21 @@
+/**
+ Zach Kamp
+ SDI Section #2
+ Go To Training Week #2
+ 12-10-2014
+ */
+//alert ("Teste!");
+
+//determine whether user is moving or not.
+var temp = prompt("What is the outside temperature?");
+var coffeeBudget = prompt ("How much money is in your pocket?")
+var warmCoffee = alert ("You should get a warm coffee!");
+var icedCoffee = alert ("You should get an iced coffee!");
+
+if(temp <= 65 && coffeeBudget >= 5){
+    warmCoffee;
+}
+
+if(temp > 65 && coffeeBudget >= 5){
+    icedCoffee;
+}
