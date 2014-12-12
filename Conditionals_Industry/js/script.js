132d08b6318d84d210d30235d63c75642f7c6ba4
zkamp <zrkamp@fullsail.edu>
2014-12-11 22:43:20 -0500

    Conditional statement
    Conditional statement

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,6 +6,9 @@
 
 //prompts for user's sedentary time.
 var workTime = prompt("How many minutes have you been working at your desk?");
+console.log("workTime")
 
+//using ternary operation
+(workTime >= 60) ? alert ("Get up and take a walk. Sitting at a desk without frequent breaks can be hazardous to your health!") : alert ("Get back to work! you have mouths to feed!");
 
 
