933013b408b6fcbdb446ad5465c654a0389a2c26
zkamp <zrkamp@fullsail.edu>
2014-12-18 11:11:09 -0500

    Set up files for week 3
    Set up files for week 3

---
+++ GoToTrainingWeek3/js/week3.js
@@ -0,0 +1,19 @@
+/*
+Zachary Kamp
+SDI Section #2
+Go To Training Week #3
+12-10-2014
+ */
+//alert("Testing to make sure we are connected");
+
+//Create code that sorts through fruit names and picks out pears
+
+//Create our "fruit bowl" array
+
+var bowlOfFruit = ["apple","banana","pear","peach"];
+
+//Console logs the whole array
+console.log(bowlOfFruit);
+
+//How to get one fruit inside of the bowl
+console.log(bowlOfFruit[1]);
