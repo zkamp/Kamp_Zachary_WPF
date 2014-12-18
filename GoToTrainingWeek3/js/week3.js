3443891f227364f669644deb6058873f6e93d62a
zkamp <zrkamp@fullsail.edu>
2014-12-18 11:17:50 -0500

    Length property of arrays
    Length property of arrays

--- GoToTrainingWeek3/js/week3.js
+++ GoToTrainingWeek3/js/week3.js
@@ -17,3 +17,14 @@ console.log(bowlOfFruit);
 
 //How to get one fruit inside of the bowl
 console.log(bowlOfFruit[1]);
+
+//How many fruits are in the bowl?
+//How many items are in the array
+//Length of the array
+//Dot Syntax - "use a period"
+console.log(bowlOfFruit.length);
+
+//What is the last index number of the array using the length property?
+//Last index number is always one less than the length.
+//bowlOfFruit.length - 1 is the last index #.
+
