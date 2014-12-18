c0fe8093ec3219d6e83a0469c609f1025db6af28
zkamp <zrkamp@fullsail.edu>
2014-12-18 18:29:09 -0500

    first prompt

--- Functions_Wacky/js/wack.js
+++ Functions_Wacky/js/wack.js
@@ -5,4 +5,9 @@
  12-18-2014
  */
 
-alert("Testing the connection");
+
+//alert("Testing the connection");
+
+//centimeter to inches conversion
+
+var cmCount = prompt("How many centimeters would you like to convert to inches?");
