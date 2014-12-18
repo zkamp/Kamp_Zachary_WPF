633738e9518f6c432812da62795c4fa2fc2737ee
zkamp <zrkamp@fullsail.edu>
2014-12-18 12:44:27 -0500

    Setup files for week 4
    Setup files for week 4

---
+++ GoToTrainingWeek4/js/week4.js
@@ -0,0 +1,13 @@
+/*
+Zach Kamp
+SDI 1412
+Go To Training Week #4
+12-17-2014
+ */
+
+//alert("testing to see if js file is connected");
+
+//Create a function that spits out a random integer between two given values.
+
+//Ask the user for a min number
+var min = prompt("Let's get a random number between two values |nPlease type in the minimum value:");
